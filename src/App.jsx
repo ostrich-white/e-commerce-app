bc916c014cb70047f7aa026a1e95ee8289ac08c4
OstrichWhite <rahimcsestudies@gmail.com>
2024-08-18 03:42:52 +0530

    Removed Notfound page, unknown routes redirect to home.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
-import NotFound from "./pages/NotFound"
 
 const App = () => (
   <Routes>
     <Route index element={<Home />}/>
-    <Route path="*" element={<NotFound />} />
+    <Route path="*" element={<Navigate to="/" />}/>
   </Routes>
 )
 

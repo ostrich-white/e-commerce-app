8004d5e7615204ab6c049f9c8834103bc168a9fb
OstrichWhite <rahimcsestudies@gmail.com>
2024-08-18 05:53:31 +0530

    Introduced Product.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import Product from "./pages/Product"
+import { ToastContainer } from "react-toastify"
 
 const App = () => (
-  <Routes>
-    <Route index element={<Home />}/>
-    <Route path="*" element={<Navigate to="/" />}/>
-  </Routes>
+  <>
+    <Routes>
+      <Route index element={<Home />}/>
+      <Route path="/products" element={<Product />}/>
+      <Route path="*" element={<Navigate to="/" />}/>
+    </Routes>
+    <ToastContainer/>
+  </>
 )
 
 
---
+++ src/pages/Product.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import AdminProduct from '../components/products/AdminProduct'
+
+const Product = () => 
+    <AdminProduct/>
+
+export default Product
